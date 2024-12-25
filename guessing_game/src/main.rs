98f592c01260361750ee508cdd66d9b48a6d0812
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2024-12-25 15:44:35 -0300

    feat: :sparkles: Add the guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,15 +1,54 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
-fn main() {
+fn guessing_game() {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    let mut guess = String::new();
+    loop {
+        println!("Please input your guess.");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let mut guess = String::new();
 
-    println!("You guessed: {}", guess);
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
+
+fn main() {
+    loop {
+        guessing_game();
+        println!("Do you want to play again? (y/n)");
+
+        let mut play_again = String::new();
+
+        io::stdin()
+            .read_line(&mut play_again)
+            .expect("Failed to read line");
+
+        play_again = play_again.trim().to_string();
+
+        if play_again != "y" {
+            break;
+        }
+    }
 }
